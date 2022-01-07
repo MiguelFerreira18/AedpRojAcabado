b660fbd2baf28dd1da255bc5a239d884f46534a7
MiguelFerreira18 <91393066+MiguelFerreira18@users.noreply.github.com>
2022-01-07 15:51:49 +0000

    heap Project FeitoV2 (AED)

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,21 @@ public class Main
         System.out.println("-------------------------");
         System.out.print("Escolha uma impressora:");
         int imp = in.nextInt();
+        while(imp<1 || imp>5)
+        {
+            System.out.println("Escolha uma impressora dentro das possiveis:");
+            imp = in.nextInt();
+        }
         System.out.println("-------------------------");
-        if(imp==0)
-            System.out.println("");
-        else {
             impMenu(imp);
             System.out.println("-------------------------");
             System.out.print("Qual ação deseja fazer:");
             int op = in.nextInt();
 
-            while (op > 0 && imp != 0) {
-                switch (op) {
+            while (op > 0 && imp != 0)
+            {
+                switch (op)
+                {
                     case 1:
                         System.out.print("titulo:");
                         String title = in.next();
@@ -41,23 +45,23 @@ public class Main
                         System.out.println("-------------------------");
                         System.out.println("Escolha uma impressora:");
                         imp = in.nextInt();
-                        op = 0;
+                        while(imp<1 || imp>5)
+                        {
+                            System.out.println("Escolha uma impressora dentro das possiveis:");
+                            imp = in.nextInt();
+                        }
                         break;
                     default:
+                        System.out.println("Essa opção não existe, selecione outra!");
+                        System.out.println("-------------------------");
                         break;
                 }
-                System.out.println("deseja mesmo sair ?(\"sim\" para sair)");
-                String sair = in.next();
-                if (sair.equalsIgnoreCase("sim"))
-                    break;
-                else {
                     impMenu(imp);
                     System.out.println("Qual ação deseja fazer");
                     op = in.nextInt();
-                }
-            }//fim do while
-        }//fim do else
-    }//fim do metodo
+            }
+        }
+
     public static void impMenu(int imp)
 {
     if(imp==0 || imp>5)
@@ -68,9 +72,9 @@ public class Main
         {
             case 1:
                System.out.println("Impressora dos Alunos:");
-               System.out.println("1-adiciona job");
-               System.out.println("2-imprimir job com menos paginas");
-               System.out.println("3-imprimir todos os jobs");
+               System.out.println("1-Adiciona job");
+               System.out.println("2-Imprimir job com menos paginas");
+               System.out.println("3-Imprimir todos os jobs");
                System.out.println("4-Sair da impressora");
                 break;
             case 2:
@@ -79,27 +83,31 @@ public class Main
                 System.out.println("2-imprimir job com menos paginas");
                 System.out.println("3-imprimir todos os jobs");
                 System.out.println("4-Sair da impressora");
+                System.out.println("0-Sair do programa");
                 break;
             case 3:
                 System.out.println("Impressora dos Funcionários dos serviços:");
-                System.out.println("1-adiciona job");
-                System.out.println("2-imprimir job com menos paginas");
-                System.out.println("3-imprimir todos os jobs");
+                System.out.println("1-Adiciona job");
+                System.out.println("2-Imprimir job com menos paginas");
+                System.out.println("3-Imprimir todos os jobs");
                 System.out.println("4-Sair da impressora");
+                System.out.println("0-Sair do programa");
                 break;
             case 4:
                 System.out.println("Impressora dos Continuos:");
-                System.out.println("1-adiciona job");
-                System.out.println("2-imprimir job com menos paginas");
-                System.out.println("3-imprimir todos os jobs");
+                System.out.println("1-Adiciona job");
+                System.out.println("2-Imprimir job com menos paginas");
+                System.out.println("3-Imprimir todos os jobs");
                 System.out.println("4-Sair da impressora");
+                System.out.println("0-Sair do programa");
                 break;
             case 5:
                 System.out.println("Impressora dos Reitores:");
-                System.out.println("1-adiciona job");
-                System.out.println("2-imprimir job com menos paginas");
-                System.out.println("3-imprimir todos os jobs");
+                System.out.println("1-Adiciona job");
+                System.out.println("2-Imprimir job com menos paginas");
+                System.out.println("3-Imprimir todos os jobs");
                 System.out.println("4-Sair da impressora");
+                System.out.println("0-Sair do programa");
                 break;
         }//fim do switch
     }//fim do else
@@ -111,6 +119,5 @@ public static void impOpc()
     System.out.println("3-Impressora dos Funcionários dos serviços");
     System.out.println("4-Impressora dos continuos");
     System.out.println("5-Impressora dos Reitores");
-    System.out.println("0-para sair");
 }//fim do metodo
 }
